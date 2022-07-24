daa006ac6236b0411167d71823d68f4f9762451c
Von Nguyen <vonnguyen041096@gmail.com>
2022-07-24 10:25:21 +0700

    shose

---
+++ main.js
@@ -0,0 +1,15 @@
+window.addEventListener('load',function(){
+    let header = this.document.querySelector('.header');
+    let headerHeight = document.querySelector('.header').offsetHeight;
+    
+    window.addEventListener('scroll',function(){
+        // console.log([window])
+        console.log(window.scrollY);
+        if(window.scrollY >= headerHeight){
+            header.classList.add('fixed');
+        }else{
+            header.classList.remove('fixed');
+
+        }
+    })
+})
